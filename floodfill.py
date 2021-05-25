520898f4124a26d4391499b1938569d440679a4e
MericGit <lawrencezhang04@gmail.com>
2021-05-25 16:51:01 -0500

    Revert "cleanup"
    This reverts commit a0880e71aa9727ec4e76a13712a658edb04def0c.

--- floodfill.py
+++ floodfill.py
@@ -1,31 +1,42 @@
 import numpy
 import server
-
-M = server.getBoardSize()[0]
-N = server.getBoardSize()[1]
+# Python3 program to implement
+# flood fill algorithm
  
-
+# Dimentions of paint screen
+M = 11
+N = 11
+ 
+# A recursive function to replace
+# previous color 'prevC' at '(x, y)'
+# and all surrounding pixels of (x, y) d
+# with new color 'newC' and
 def floodFillUtil(screen, x, y, prevC, newC):
      
-
+    # Base cases
     if (x < 0 or x >= M or y < 0 or
         y >= N or screen[x][y] != prevC or
         screen[x][y] == newC):
         return
  
-
+    # Replace the color at (x, y)
     screen[x][y] = newC
  
-
+    # Recur for north, east, south and west
     floodFillUtil(screen, x + 1, y, prevC, newC)
     floodFillUtil(screen, x - 1, y, prevC, newC)
     floodFillUtil(screen, x, y + 1, prevC, newC)
     floodFillUtil(screen, x, y - 1, prevC, newC)
  
+# It mainly finds the previous color on (x, y) and
+# calls floodFillUtil()
 def floodFill(screen, x, y, newC):
     prevC = screen[x][y]
     floodFillUtil(screen, x, y, prevC, newC)
  
+# Driver Code
+
+
 
 
 def safety(yHead,xHead,clone,y,x,num):
